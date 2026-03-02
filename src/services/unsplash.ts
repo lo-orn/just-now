949bb18225d61e4d9fb723b87d147100f0ac0deb
Lo <lovisaornestal@gmail.com>
2026-03-02 08:27:06 +0100

    changed unsplash index to 0 to avoid undefined results

--- src/services/unsplash.ts
+++ src/services/unsplash.ts
@@ -2,22 +2,17 @@
 const API_KEY = import.meta.env.VITE_UNSPLASH_ACCESS_KEY
 
 export const getMoodImage = async (searchQuery: string) => {
+
   const response = await fetch(`https://api.unsplash.com/search/photos?query=${searchQuery}&client_id=${API_KEY}`)
   const data = await response.json()
 
     if (!data.results.length) {
     return { url: "", photographer: "", photographerLink: ""} 
 }
-  const index = Math.floor(Math.random()* data.results.length)
-    if (!data.results?.[index]?.urls) {
-    return { url: "", photographer: "", photographerLink: "" }
-}
-console.log(data.results[index].user.links.html)
-  const url = data.results[index].urls.regular
-  const photographer = data.results[index].user.name
-  const photographerLink = data.results[index].user.links.html
+
+  const url = data.results[0].urls.regular
+  const photographer = data.results[0].user.name
+  const photographerLink = data.results[0].user.links.html
 
   return {url, photographer, photographerLink}
-
- 
-}
+} 
